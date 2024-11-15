351f290982f7f78ff90072de665ac533309606f5
BenBraniff <ben.braniff37@gmail.com>
2024-11-15 16:03:40 -0500

    fixed the weird formatting and added leaderboards plus rewards tables

--- src/components/CompetitionsPage/CompetitionsPage2.jsx
+++ src/components/CompetitionsPage/CompetitionsPage2.jsx
@@ -33,6 +33,56 @@ const CompetitionsPage2 = () => {
                     <button class="join-button">Join</button>
                 </div>
                 <p class="date-range">Open: Now - Fri Nov. 15</p>
+                <div class="leaderboard">
+                    <h3>Leaderboard</h3>
+                    <table>
+                        <tr>
+                            <td>1.</td>
+                            <td>315 lb</td>
+                            <td>Ben</td>
+                        </tr>
+                        <tr>
+                            <td>2.</td>
+                            <td>275 lb</td>
+                            <td>Alex</td>
+                        </tr>
+                        <tr>
+                            <td>3.</td>
+                            <td>270 lb</td>
+                            <td>Brett</td>
+                        </tr>
+                        <tr>
+                            <td>4.</td>
+                            <td>225 lb</td>
+                            <td>James</td>
+                        </tr>
+                        <tr>
+                            <td>5.</td>
+                            <td>205 lb</td>
+                            <td>Max</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="rewards">
+                    <h3>Rewards</h3>
+                    <table>
+                        <tr>
+                            <td>1.</td>
+                            <td>$25 gift card</td>
+                        </tr>
+                        <tr>
+                            <td>2.</td>
+                            <td>$15 gift card</td>
+                        </tr>
+                        <tr>
+                            <td>3.</td>
+                            <td>$10 gift card</td>
+                        </tr>
+                        <tr>
+                            fit cash for all
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
